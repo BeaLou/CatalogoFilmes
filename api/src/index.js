0a5ad3e2d3f730ce072d140a7783069e48e0b541
Beatriz Ramos Lourenco <beatrizlourenco1912@gmail.com>
2022-07-05 23:57:26 -0300

    rota de login criada

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,6 @@
 //importando  as dependencias e variaveis de ambiente que serão usadas
 import 'dotenv/config'
-import { conn } from './repository/connection.js'
-
+import usuarioController from './controller/usuarioController.js'
 import express from 'express'
 import cors from 'cors'
 
@@ -9,7 +8,8 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-
+//configuração de endpoints
+server.use(usuarioController);
 
 
 server.listen(process.env.PORT, () => console.log(`API conectada na porta ${process.env.PORT}`));
