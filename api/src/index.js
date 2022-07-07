c66e78e1dbd0d403801b288c1bedcf08fa73bd88
Beatriz Ramos Lourenco <beatrizlourenco1912@gmail.com>
2022-07-06 21:47:49 -0300

    add crud filme

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 //importando  as dependencias e variaveis de ambiente que serão usadas
 import 'dotenv/config'
 import usuarioController from './controller/usuarioController.js'
+import filmeController from './controller/filmeController.js'
 import express from 'express'
 import cors from 'cors'
 
@@ -8,8 +9,11 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+///liberar arquivos
+server.use('/storage/image', express.static('storage/image'));
+
 //configuração de endpoints
 server.use(usuarioController);
-
+server.use(filmeController);
 
 server.listen(process.env.PORT, () => console.log(`API conectada na porta ${process.env.PORT}`));
