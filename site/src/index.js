39602ec4125669d3251cab67458323e32e3d45e0
Beatriz Ramos Lourenco <beatrizlourenco1912@gmail.com>
2022-07-12 11:52:32 -0300

    add react a tela de cadastro de filme

--- site/src/index.js
+++ site/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
 
 import './index.scss';
-
+import 'react-toastify/dist/ReactToastify.css';
 import Routes from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>
-);
+root.render( <
+    React.StrictMode >
+    <
+    ToastContainer / >
 
+    <
+    Routes / >
+    <
+    /React.StrictMode>
+);
--- site/src/pages/cadastrar/index.js
+++ site/src/pages/cadastrar/index.js
@@ -1,10 +1,32 @@
 import Menu from '../../components/menu'
 import Cabecalho from '../../components/cabecalho'
-
+import { useState } from 'react'
 import './index.scss'
+import storage from 'local-storage'
+import { cadastrarFilme, enviarImagemFilme } from '../../api/filmeApi'
 
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Index() {
+    const [nome, setNome] = useState('')
+    const [avaliacao, setAvaliacao] = useState('')
+    const [lancamento, setLancamento] = useState(0)
+    const [disponivel, setDisponivel] = useState(false)
+    const [sinopse, setSinopse] = useState('')
+
+    async function salvarClick() {
+        try {
+            const usuario = storage('usuario-logado').id;
+            const r = await cadastrarFilme(nome, avaliacao, lancamento, disponivel, sinopse, usuario);
+
+            toast('Filme cadastrado com sucesso');
+        } catch (err) {
+            toast.error(err.response.data.erro);
+        }
+
+    }
+
     return ( <
         main className = 'page page-cadastrar' >
         <
@@ -21,7 +43,7 @@ export default function Index() {
         <
         h1 className = 'titulo' >
         <
-        span > & nbsp; < /span>Cadastrar Novo Filme  <
+        span > < /span>Cadastrar Novo Filme  <
         /h1> <
         div className = 'form-colums' >
         <
@@ -40,29 +62,37 @@ export default function Index() {
         <
         label > Nome: < /label> <
         input type = 'text'
-        placeholder = 'Nome do filme' / >
-        <
+        placeholder = 'Nome do filme'
+        value = { nome }
+        onChange = { e => setNome(e.target.value) }
+        /> <
         /div> <
         div className = 'form-row' >
         <
         label > Avaliação: < /label> <
         input type = 'number'
-        placeholder = '0' / >
-        <
+        placeholder = '0'
+        value = { avaliacao }
+        onChange = { e => setAvaliacao(e.target.value) }
+        /> <
         /div> <
         div className = 'form-row' >
         <
         label > Lançamento: < /label> <
         input type = 'date'
-        value = '2022-01-01' / >
-        <
+        value = { lancamento }
+        onChange = { e => setLancamento(e.target.value) }
+        /> <
         /div> <
         br / >
         <
         div className = 'form-row' >
         <
         label > < /label> <
-        input type = 'checkbox' / > & nbsp; Disponível <
+        input type = 'checkbox'
+        checked = { disponivel }
+        onChange = { e => setDisponivel(e.target.checked) }
+        /> &nbsp; Disponível <
         /div> <
         /div> <
         div >
@@ -73,8 +103,10 @@ export default function Index() {
         <
         label style = {
             { marginTop: '13px' } } > Sinopse: < /label> <
-        textarea placeholder = 'Sinopse do filme' / >
-        <
+        textarea placeholder = 'Sinopse do filme'
+        value = { sinopse }
+        onChange = { e => setSinopse(e.target.value) }
+        /> <
         /div> <
         br / >
         <
@@ -85,7 +117,7 @@ export default function Index() {
         label > < /label> <
         div className = 'btnSalvar' >
         <
-        button > SALVAR < /button>     <
+        button onClick = { salvarClick } > SALVAR < /button>     <
         /div> <
         /div> <
         /div> <
