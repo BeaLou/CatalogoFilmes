3e668b33e4a355c4aa6c94a0553517421779aeed
Beatriz Ramos Lourenco <beatrizlourenco1912@gmail.com>
2022-07-12 14:57:57 -0300

    add imagem

--- site/src/pages/cadastrar/index.js
+++ site/src/pages/cadastrar/index.js
@@ -14,18 +14,36 @@ export default function Index() {
     const [lancamento, setLancamento] = useState(0)
     const [disponivel, setDisponivel] = useState(false)
     const [sinopse, setSinopse] = useState('')
+    const [imagem, setImagem] = useState()
+
 
     async function salvarClick() {
         try {
+            if (!imagem)
+                throw new Error('Escolha a capa do filme');
+
             const usuario = storage('usuario-logado').id;
-            const r = await cadastrarFilme(nome, avaliacao, lancamento, disponivel, sinopse, usuario);
+            const novoFilme = await cadastrarFilme(nome, avaliacao, lancamento, disponivel, sinopse, usuario);
+            await enviarImagemFilme(novoFilme.id, imagem);
 
             toast('Filme cadastrado com sucesso');
         } catch (err) {
-            toast.error(err.response.data.erro);
+            if (err.response)
+                toast.error(err.response.data.erro);
+            else
+                toast.error(err.message);
         }
 
     }
+
+    function escolherImagem() {
+        document.getElementById('imagemcapa').click();
+    }
+
+    function mostrarImagem() {
+        return URL.createObjectURL(imagem);
+    }
+
 
     return ( <
         main className = 'page page-cadastrar' >
@@ -43,18 +61,31 @@ export default function Index() {
         <
         h1 className = 'titulo' >
         <
-        span > < /span>Cadastrar Novo Filme  <
-        /h1> <
+        span > < /span>Cadastrar Novo Filme  < /
+        h1 > <
         div className = 'form-colums' >
         <
         div >
         <
-        div className = 'upload-capa' >
-        <
-        img src = "/assets/images/icon-upload.svg"
-        alt = "" / >
-        <
-        /div> <
+        div className = 'upload-capa'
+        onClick = { escolherImagem } > {!imagem &&
+            <
+            img src = "/assets/images/icon-upload.svg"
+            alt = "" / >
+        }
+
+        {
+            imagem &&
+                <
+                img className = 'imagemCapa'
+            src = { mostrarImagem() }
+            alt = '' / >
+        } <
+        input type = 'file'
+        id = 'imagemcapa'
+        onChange = { e => setImagem(e.target.files[0]) }
+        /> < /
+        div > <
         /div> <
         div >
         <
@@ -65,8 +96,8 @@ export default function Index() {
         placeholder = 'Nome do filme'
         value = { nome }
         onChange = { e => setNome(e.target.value) }
-        /> <
-        /div> <
+        /> < /
+        div > <
         div className = 'form-row' >
         <
         label > Avaliação: < /label> <
@@ -74,16 +105,16 @@ export default function Index() {
         placeholder = '0'
         value = { avaliacao }
         onChange = { e => setAvaliacao(e.target.value) }
-        /> <
-        /div> <
+        /> < /
+        div > <
         div className = 'form-row' >
         <
         label > Lançamento: < /label> <
         input type = 'date'
         value = { lancamento }
         onChange = { e => setLancamento(e.target.value) }
-        /> <
-        /div> <
+        /> < /
+        div > <
         br / >
         <
         div className = 'form-row' >
@@ -92,22 +123,24 @@ export default function Index() {
         input type = 'checkbox'
         checked = { disponivel }
         onChange = { e => setDisponivel(e.target.checked) }
-        /> &nbsp; Disponível <
-        /div> <
+        /> &nbsp; Disponível < /
+        div > <
         /div> <
         div >
         <
         div className = 'form-row'
         style = {
-            { alignItems: 'flex-start' } } >
+            { alignItems: 'flex-start' }
+        } >
         <
         label style = {
-            { marginTop: '13px' } } > Sinopse: < /label> <
+            { marginTop: '13px' }
+        } > Sinopse: < /label> <
         textarea placeholder = 'Sinopse do filme'
         value = { sinopse }
         onChange = { e => setSinopse(e.target.value) }
-        /> <
-        /div> <
+        /> < /
+        div > <
         br / >
         <
         br / >
@@ -117,14 +150,14 @@ export default function Index() {
         label > < /label> <
         div className = 'btnSalvar' >
         <
-        button onClick = { salvarClick } > SALVAR < /button>     <
-        /div> <
-        /div> <
-        /div> <
-        /div> <
-        /section> <
-        /div> <
-        /div> <
+        button onClick = { salvarClick } > SALVAR < /button>     < /
+        div > <
+        /div> < /
+        div > <
+        /div> < /
+        section > <
+        /div> < /
+        div > <
         /main>
     )
 }
