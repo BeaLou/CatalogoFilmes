99451f3d09705cc69d48bf40da58791bdae685cf
Beatriz Ramos Lourenco <beatrizlourenco1912@gmail.com>
2022-07-12 20:54:10 -0300

    add consultar card

--- site/src/pages/consultarCard/index.js
+++ site/src/pages/consultarCard/index.js
@@ -1,10 +1,69 @@
 import Menu from '../../components/menu'
 import Cabecalho from '../../components/cabecalho'
-
+import { useState, useEffect } from "react";
+import { listarTodosFilmes, buscarFilmesPorNome, removerFilme } from "../../api/filmeApi";
+import "./index.scss";
+import {confirmAlert } from 'react-confirm-alert';
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 import './index.scss'
 
 
 export default function Index() {
+    const [filmes, setFilmes] = useState([]);
+    const [filtro, setFiltro] = useState("");
+  
+    const navigate = useNavigate();
+  
+    function abrirDetalhes(id){
+      navigate(`/admin/detalhe/${id}`);
+    }
+  
+    function editarFilme(id){
+      navigate(`/admin/alterar/${id}`);
+    }
+  
+    async function filtrar() {
+      const resp = await buscarFilmesPorNome(filtro);
+      setFilmes(resp);
+    }
+    async function carregarTodosFilmes() {
+      const resp = await listarTodosFilmes();
+      setFilmes(resp);
+    }
+  
+  
+    async function removerFilmeClick(id, nome){
+  
+      confirmAlert({
+          title: 'Remover filme',
+          message: `Deseja remover o filme ${nome}`,
+          buttons: [
+            {
+              label: 'Sim',
+              onClick: async () => {
+                  const resposta = await removerFilme(id, nome);
+                  if(filtro === '')
+                      carregarTodosFilmes();
+                  else 
+                      filtrar();  
+                  
+                      toast('filme removido');
+              }
+            },
+            {
+              label: 'Não',
+            }
+          ]
+        });
+      
+    }
+  
+    useEffect(() => {
+      carregarTodosFilmes();
+    }, []);
+  
+
     return (
         <main className='page page-consultar'>
             <Menu />
@@ -22,34 +81,32 @@ export default function Index() {
 
                     <div className='card-container'>
 
-
+                    {filmes.map(item =>
                         <div className='comp-card'>
-                            <div className='card'>
-                                <div className='acoes'>
+                        <div className='card' key={item.id} onClick={() => abrirDetalhes(item.id)}>
+                            <div className='acoes'>
+                                <img src='/assets/images/icon-editar.svg' alt='editar' onClick={e =>{ 
+                      e.stopPropagation();
+                      editarFilme(item.id)
+                    }} />                                
+                                <img src='/assets/images/icon-remover.svg' alt='remover' onClick={e =>{ 
+                      e.stopPropagation();removerFilmeClick(item.id, item.nome)
+                      }}/>                                
+                            </div>
 
-                                    <img src='/assets/images/icon-editar.svg' alt='editar' />
-                                    
-                                    <img src='/assets/images/icon-remover.svg' alt='remover' />
-                                    
-                                </div>
-                                <div>
-                                    <div className='sigla'>H</div>
-                                    <div className='filme'>Harry Potter e a Pedra Filosofal </div>
-                                    <div className='lancamento'>Maio/2002</div>
-                                </div>
-                                <div>
-                                    <div className='avaliacao'>Avaliação: 8,0</div>
-                                    <div className='disponivel'>Disponível</div>
-                                </div>
+                            <div>
+                                <div className='sigla'>{item.nome.substr(0,1)}</div>
+                                <div className='filme'>{item.nome}</div>
+                                <div className='lancamento'>{item.lancamento}</div>
+                            </div>
+                            <div>
+                                <div className='avaliacao'>Avaliação:{item.avaliacao}</div>
+                                <div className='disponivel'>{item.disponivel ? 'disponivel': 'indisponivel'}</div>
                             </div>
                         </div>
-
-                        
-                        
                     </div>
-
-
-                    
+                    )};                     
+                    </div>
                 </div>
             </div>
         </main>
